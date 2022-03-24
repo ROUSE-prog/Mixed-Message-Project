502dc3e910d46b3e04c2f7f72eed493648da2f4e
ROUSE-prog <stevenr2049@gmail.com>
2022-03-24 19:48:28 -0400

    Completeled the Project for the Mixed message.

--- MixedMessages/MossVariety.js
+++ MixedMessages/MossVariety.js
@@ -1,3 +1,46 @@
-x = 2 + 5;
+let userName = '';
 
-console.log(x);
+if(userName === 'Jane') {
+  console.log('Hello, Jane!');
+} else {
+  console.log('Hello');
+}
+
+let userQuestion = 'Which Moss am I?';
+
+console.log(userQuestion);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let moss = '';
+
+switch(randomNumber) {
+  case 0:
+  console.log('Like the FIRE MOSS, You are Bold!');
+  break;
+  case 1:
+  console.log('You are dynamic and adventurous like the SHEET MOSS');
+  break;
+  case 2:
+  console.log('Your Harir is beautiful and Rojo like the Warnstof Peat Moss!');
+  break;
+  case 3:
+  console.log('I bet you would get along with the Warnstof Peat Moss!');
+  break;
+  case 4:
+  console.log('Google the Warnstof Peat Moss... It looks like you ;)');
+  break;
+  case 5:
+  console.log('Hmmm.. the Warnstof Peat Moss looks like you but a bit green.');
+  break;
+  case 6:
+  console.log('Juniper Moss thrives in the sunlight I bet you do too!');
+  break;
+  case 7:
+  console.log('You probably needs more water like the Hoary Fringe Moss');
+  break;
+
+}
+
+console.log(moss);
+
